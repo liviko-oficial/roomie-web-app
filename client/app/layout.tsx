cc03edf62f9eb1d84b474ce111aa2ac5e370b6c8
nasa14119 <nico.amaya0205@gmail.com>
2025-07-16 14:43:33 -0600

    feat(layout): Adding poppins to the layout commponent

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,14 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const font_main = Poppins({
+  variable: "--font-main",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
 });
 
@@ -23,10 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="es">
+      <body className={`${font_main.variable} dark`} data-theme="light">
         {children}
       </body>
     </html>
