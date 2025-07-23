c495d1c374ff9024029aa4db26ef4954d0809b96
nasa14119 <nico.amaya0205@gmail.com>
2025-07-23 13:03:28 -0600

    merge(dev): changing to how is implemented

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,8 @@
 import Hero from "@/modules/home/sections/Hero";
 import Nav from "@/modules/home/sections/Nav";
 import ThemeToggle from "@/modules/theme/components/ThemeToggle";
-import styles from "@/modules/home/styles.module.css";
+import styles from "../modules/home/styles.module.css";
+
 export default function Home() {
   return (
     <>
