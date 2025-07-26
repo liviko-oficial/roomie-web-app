844b17abb044f4df218f71e9765d69beb44b7cb4
nasa14119 <nico.amaya0205@gmail.com>
2025-07-26 12:15:13 -0600

    fix(theme): adding so tailwind can add styles in dark mode

--- client/modules/home/sections/Nav.tsx
+++ client/modules/home/sections/Nav.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Nav = () => {
   return (
     <nav className="flex px-4 py-2 justify-center lg:justify-start sticky top-0">
-      <div className="size-fit p-2 rounded-[99999px] bg-[hsl(0,0%,90%)]">
+      <div className="size-fit p-2 rounded-[99999px] dark:bg-[hsl(0,0%,90%)]">
         <Logo className="size-12" />
       </div>
     </nav>
