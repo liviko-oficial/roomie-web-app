48c746011746a258dc79d7e39bf6bda258194d26
Angel Montemayor Davila <a01785840@tec.mx>
2025-10-03 19:35:53 -0600

    add Users form end point

--- server/src/user/routes/index.ts
+++ server/src/user/routes/index.ts
@@ -2,8 +2,13 @@ import { Router } from "express";
 import register from "./resgister";
 import login from "./login";
 import password_recovery from "./password_recovery";
+import user_form from "./user_form";
+
 const app = Router();
+
 app.use("/", register);
 app.use("/", login);
 app.use("/", password_recovery);
+app.use("/", user_form);
+
 export default app;
