960d0d232ed3f6948fe53cfae93e2a970fbcecba
andrea0615 <A01641193@tec.mx>
2026-04-13 12:30:49 -0600

    change page.tsx for the search page thing

--- client/app/(main)/properties/page.tsx
+++ client/app/(main)/properties/page.tsx
@@ -2,5 +2,10 @@
 import SearchPage from '@/modules/home/components/SearchPage';
 
 export default function Home() {
-  return (<SearchPage onNavigate={() => {}}></SearchPage>);
+  return (
+    <SearchPage 
+      onNavigate={() => {}} 
+      initialFilters={{}} 
+    />
+  );
 }
