5798c8de4e42032b738e0d0fc9b6dd6a19615c96
andrea0615 <A01641193@tec.mx>
2026-04-13 12:09:28 -0600

    comentar appproviders and navbar

--- client/app/(main)/page.tsx
+++ client/app/(main)/page.tsx
@@ -4,9 +4,9 @@ import HowItWorks from "@/modules/home/sections/HowItWorks";
 import Testimonials from "@/modules/home/sections/Testimonials";
 import CallToAction from "@/modules/home/sections/CallToAction";
 import Footer from "@/modules/home/sections/Footer";
-import NavBar from "@/modules/home/sections/NavBar";
+//import NavBar from "@/modules/home/sections/NavBar";
 import SearchBar from "@/modules/home/components/SearchBar";
-import { AppProviders } from "@/modules/context_files/AppProviders";
+//import { AppProviders } from "@/modules/context_files/AppProviders";
 
 export default function Home() {
   return (
